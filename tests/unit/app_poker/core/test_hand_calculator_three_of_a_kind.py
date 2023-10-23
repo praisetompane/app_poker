7a1c41c3ffb666e09f3e0e7f47ac2e90b2400c78
Praise Tompane <praisetompane@icloud.com>
2023-10-23 15:13:57 +0200

    test(core): three of a kind
    implment test for three of a kind

--- tests/unit/app_poker/core/test_hand_calculator_three_of_a_kind.py
+++ tests/unit/app_poker/core/test_hand_calculator_three_of_a_kind.py
@@ -3,11 +3,9 @@ from app_poker.config.high_game.hand_rank import HandRank
 from app_poker.core.hand_rank_calculator.calculator import HandRankCalculator
 from app_poker.model.card import Card
 from app_poker.model.card_values import (
-    ace_card_value,
-    jack_card_value,
-    king_card_value,
     queen_card_value,
     ten_card_value,
+    four_card_value,
 )
 from app_poker.model.hand import Hand
 from app_poker.model.suit import Suit
@@ -16,5 +14,13 @@ hand_calculator = HandRankCalculator()
 
 
 def test_calculate_highest_hand_rank_correctly_returns_three_of_a_kind():
-    # TODO: implement tests
-    pass
+    cards = [
+        Card(queen_card_value, Suit.Clubs, card_ranks[queen_card_value]),
+        Card(queen_card_value, Suit.Diamonds, card_ranks[queen_card_value]),
+        Card(queen_card_value, Suit.Hearts, card_ranks[queen_card_value]),
+        Card(ten_card_value, Suit.Spades, card_ranks[ten_card_value]),
+        Card(four_card_value, Suit.Clubs, card_ranks[four_card_value]),
+    ]
+    hand = Hand(cards)
+    expected_result = HandRank.THREE_OF_A_KIND
+    assert expected_result == hand_calculator.calculate_highest_hand_rank(hand)
