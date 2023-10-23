15b0ef254cc0428cf66b578f6306bfe826dc8442
Praise Tompane <praisetompane@icloud.com>
2023-10-23 15:31:01 +0200

    test(core) two pair
    implement tests for Two Pair hand rank

--- tests/unit/app_poker/core/test_hand_calculator_two_pair.py
+++ tests/unit/app_poker/core/test_hand_calculator_two_pair.py
@@ -3,11 +3,9 @@ from app_poker.config.high_game.hand_rank import HandRank
 from app_poker.core.hand_rank_calculator.calculator import HandRankCalculator
 from app_poker.model.card import Card
 from app_poker.model.card_values import (
-    ace_card_value,
     jack_card_value,
-    king_card_value,
-    queen_card_value,
     ten_card_value,
+    nine_card_value,
 )
 from app_poker.model.hand import Hand
 from app_poker.model.suit import Suit
@@ -16,5 +14,13 @@ hand_calculator = HandRankCalculator()
 
 
 def test_calculate_highest_hand_rank_correctly_returns_two_pair():
-    # TODO: implement tests
-    pass
+    cards = [
+        Card(ten_card_value, Suit.Clubs, card_ranks[ten_card_value]),
+        Card(ten_card_value, Suit.Diamonds, card_ranks[ten_card_value]),
+        Card(nine_card_value, Suit.Hearts, card_ranks[nine_card_value]),
+        Card(nine_card_value, Suit.Spades, card_ranks[nine_card_value]),
+        Card(jack_card_value, Suit.Clubs, card_ranks[jack_card_value]),
+    ]
+    hand = Hand(cards)
+    expected_result = HandRank.TWO_PAIR
+    assert expected_result == hand_calculator.calculate_highest_hand_rank(hand)
