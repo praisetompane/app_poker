4351c08c8281fabb52aa34363d819863c55332c2
Praise Tompane <praisetompane@icloud.com>
2023-10-23 15:45:55 +0200

    feat(core): one pair
    implement one pair hand rank detection and curl and postman end to end test util.

--- src/app_poker/core/hand_rank_calculator/calculator.py
+++ src/app_poker/core/hand_rank_calculator/calculator.py
@@ -1,13 +1,13 @@
 from app_poker.config.high_game.hand_rank import HandRank
-from app_poker.core.hand_rank_calculator.royal_flush import is_royal_flush
-from app_poker.core.hand_rank_calculator.straight_flush import is_straight_flush
-from app_poker.core.hand_rank_calculator.straight import is_straight
+from app_poker.core.hand_rank_calculator.flush import is_flush
 from app_poker.core.hand_rank_calculator.four_of_a_kind import is_four_of_a_kind
+from app_poker.core.hand_rank_calculator.full_house import is_full_house
+from app_poker.core.hand_rank_calculator.one_pair import is_a_one_pair
+from app_poker.core.hand_rank_calculator.royal_flush import is_royal_flush
+from app_poker.core.hand_rank_calculator.straight import is_straight
+from app_poker.core.hand_rank_calculator.straight_flush import is_straight_flush
 from app_poker.core.hand_rank_calculator.three_of_a_kind import is_three_of_a_kind
 from app_poker.core.hand_rank_calculator.two_pair import is_a_two_pair
-from app_poker.core.hand_rank_calculator.full_house import is_full_house
-
-from app_poker.core.hand_rank_calculator.flush import is_flush
 from app_poker.model.hand import Hand
 
 
@@ -54,7 +54,8 @@ class HandRankCalculator:
             return HandRank.THREE_OF_A_KIND
         elif is_a_two_pair(cards):
             return HandRank.TWO_PAIR
-        # elif is_one_pair(cards):
+        elif is_a_one_pair(cards):
+            return HandRank.ONE_PAIR
         else:
             return HandRank.HIGH_CARD
 
