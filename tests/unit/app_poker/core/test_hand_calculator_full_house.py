53022ea1ca17fe8f520196f7a26ab2b1efcbb2e1
Praise Tompane <praisetompane@icloud.com>
2023-10-23 14:51:50 +0200

    test(core): full house
    implment test for full house

--- tests/unit/app_poker/core/test_hand_calculator_full_house.py
+++ tests/unit/app_poker/core/test_hand_calculator_full_house.py
@@ -3,11 +3,8 @@ from app_poker.config.high_game.hand_rank import HandRank
 from app_poker.core.hand_rank_calculator.calculator import HandRankCalculator
 from app_poker.model.card import Card
 from app_poker.model.card_values import (
-    ace_card_value,
-    jack_card_value,
     king_card_value,
-    queen_card_value,
-    ten_card_value,
+    seven_card_value,
 )
 from app_poker.model.hand import Hand
 from app_poker.model.suit import Suit
@@ -16,5 +13,13 @@ hand_calculator = HandRankCalculator()
 
 
 def test_calculate_highest_hand_rank_correctly_returns_full_house():
-    # TODO: implement tests
-    pass
+    cards = [
+        Card(king_card_value, Suit.Clubs, card_ranks[king_card_value]),
+        Card(king_card_value, Suit.Diamonds, card_ranks[king_card_value]),
+        Card(king_card_value, Suit.Hearts, card_ranks[king_card_value]),
+        Card(seven_card_value, Suit.Spades, card_ranks[seven_card_value]),
+        Card(seven_card_value, Suit.Clubs, card_ranks[seven_card_value]),
+    ]
+    hand = Hand(cards)
+    expected_result = HandRank.FULL_HOUSE
+    assert expected_result == hand_calculator.calculate_highest_hand_rank(hand)
